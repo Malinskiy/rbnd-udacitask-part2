2207b9e4df757ea0a71d8fd2bbf99fd0d6494ca5
Anton Malinskiy <anton@malinskiy.com>
2016-06-06 22:05:13 +0700

    Extract format_date to Listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,8 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_date(fallback, *dates)
+    dates.map { |date| date.nil? ? fallback : date.strftime('%D') }.join(' -- ')
+  end
 end
